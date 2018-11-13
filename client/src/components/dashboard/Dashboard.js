6e1f9ca7203b7c4504f0e1c5cea916bdd45752b5
unknown <adamalshaick@gmail.com>
2018-11-13 14:10:55 +0100

    added dashboard

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profileActions";
+
+class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getCurrentProfile();
+  }
+  render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent;
+
+    if (profile === null || loading) {
+      dashboardContent = <p>Loading...</p>;
+    } else {
+      // Check if logged in user has profile data
+      if (Object.keys(profile).length > 0) {
+        dashboardContent = <div>disp profile will be</div>;
+      } else {
+        // User is logged in but has no profile
+        dashboardContent = (
+          <div>
+            <p> Welcome {user.name}</p>
+            <p> Create a profile</p>
+            <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create profile
+            </Link>
+          </div>
+        );
+      }
+    }
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <p>Dashboard</p>
+            {dashboardContent}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
