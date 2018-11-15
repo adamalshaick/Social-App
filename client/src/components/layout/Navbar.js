25c073262f37c927a8b0a02b83976efc9bcad4cc
unknown <adamalshaick@gmail.com>
2018-11-15 02:31:28 +0100

    edit profile functionality

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -17,6 +17,11 @@ class Navbar extends Component {
     const authLinks = (
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">
+            Dashboard
+          </Link>
+        </li>
+        <li className="nav-item">
           <a
             href=""
             onClick={this.onLogoutClick.bind(this)}
@@ -39,6 +44,8 @@ class Navbar extends Component {
           <Link className="nav-link" to="/login">
             Login
           </Link>
+        </li>
+        <li>
           <Link className="nav-link" to="/register">
             Sign Up
           </Link>
