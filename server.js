75127d9c2bf14fea5426ba4fef395c5ab8c7afec
unknown <adamalshaick@gmail.com>
2019-02-04 13:56:07 +0100

    ?

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const socket = require("socket.io");
 const path = require("path");
 
 const users = require("./routes/api/users");
