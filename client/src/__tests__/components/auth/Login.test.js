59247fd48c419f8e397c82d45650c08cccbac07f
unknown <adamalshaick@gmail.com>
2019-02-21 15:48:29 +0100

    auth components tests

---
+++ client/src/__tests__/components/auth/Login.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Login } from "../../../components/auth/Login";
+import { shallow } from "enzyme";
+
+const mockLoginfn = jest.fn();
+const Props = {
+  auth: {
+    isAuthenticated: false
+  },
+  errors: {}
+};
+let wrapper;
+
+wrapper = shallow(<Login {...Props} loginUser={mockLoginfn} />);
+
+it("displays form", () => {
+  expect(wrapper.find("form").length).toEqual(1);
+});
+
+it("should call the mock login function", () => {
+  wrapper.find("form").simulate("submit", { preventDefault() {} });
+  expect(mockLoginfn.mock.calls.length).toBe(1);
+});
+
+describe("login action", () => {
+  wrapper
+    .find("#email")
+    .simulate("change", { target: { name: "email", value: "test@gmail.com" } });
+
+  wrapper.find("#password").simulate("change", {
+    target: { name: "password", value: "passwordTest" }
+  });
+
+  it("calls function with correct data", () => {
+    expect(mockLoginfn.mock.calls[0][0]).toEqual({
+      email: "test@gmail.com",
+      password: "passwordTest"
+    });
+  });
+});
---
+++ client/src/__tests__/components/auth/Register.test.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Register } from "../../../components/auth/Register";
+import { shallow } from "enzyme";
+
+const mockRegisterfn = jest.fn();
+const Props = {
+  auth: {
+    isAuthenticated: false
+  },
+  errors: {}
+};
+let wrapper;
+
+wrapper = shallow(<Register {...Props} registerUser={mockRegisterfn} />);
+
+it("displays form", () => {
+  expect(wrapper.find("form").length).toEqual(1);
+});
+
+it("should call the mock register function", () => {
+  wrapper.find("form").simulate("submit", { preventDefault() {} });
+  expect(mockRegisterfn.mock.calls.length).toBe(1);
+});
+
+describe("register action", () => {
+  wrapper
+    .find("#name")
+    .simulate("change", { target: { name: "name", value: "testName" } });
+
+  wrapper
+    .find("#email")
+    .simulate("change", { target: { name: "email", value: "test@gmail.com" } });
+
+  wrapper.find("#password").simulate("change", {
+    target: { name: "password", value: "passwordTest" }
+  });
+
+  wrapper.find("#password2").simulate("change", {
+    target: { name: "password2", value: "password2Test" }
+  });
+
+  it("calls function with correct data", () => {
+    expect(mockRegisterfn.mock.calls[0][0]).toEqual({
+      name: "testName",
+      email: "test@gmail.com",
+      password: "passwordTest",
+      password2: "password2Test"
+    });
+  });
+});
