5371cdf2ab178ea050d3214ffa43d908626c2565
unknown <adamalshaick@gmail.com>
2019-02-23 22:47:32 +0100

    styling for components

---
+++ client/src/components/comments/CommentForm.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import { addComment } from "../../actions/postActions";
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.errors) {
+      this.setState({ errors: newProps.errors });
+    }
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const { user } = this.props.auth;
+    const { postId } = this.props;
+
+    const newComment = {
+      text: this.state.text,
+      name: user.name,
+      avatar: user.avatar
+    };
+
+    this.props.addComment(postId, newComment);
+    this.setState({ text: "" });
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  render() {
+    return (
+      <div className="col-12">
+        <form onSubmit={this.onSubmit}>
+          <TextAreaFieldGroup
+            className="form-control form-control-lg"
+            placeholder="Make a comment..."
+            name="text"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+
+          <div className="float-right">
+            <button type="submit" className="btn btn-outline-primary btn-sm">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+CommentForm.propTypes = {
+  addPost: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { addComment }
+)(CommentForm);
---
+++ client/src/components/comments/CommentItem.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { deleteComment } from "../../actions/postActions";
+import Link from "react-router-dom/Link";
+
+class CommentItem extends Component {
+  onDeleteClick(postId, commentId) {
+    this.props.deleteComment(postId, commentId);
+  }
+
+  render() {
+    const { comment, postId, auth } = this.props;
+    return (
+      <div className=" mb-3">
+        <div className="row mt-3">
+          <Link to="">
+            <img
+              style={{ width: "40px" }}
+              className="rounded-circle  ml-4"
+              src={comment.avatar}
+              alt=""
+            />
+          </Link>
+
+          <div className="text-center mt-2 ml-2">{comment.name}</div>
+
+          <div className="col-12">
+            <div style={{ fontSize: "0.9rem" }} className="float-left ml-2">
+              {comment.text}
+            </div>
+          </div>
+
+          {comment.user === auth.user.id ? (
+            <button
+              onClick={this.onDeleteClick.bind(this, postId, comment._id)}
+              type="button"
+              className="btn btn-outline-danger btn-sm  mr-1"
+            >
+              Delete
+            </button>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+}
+
+CommentItem.propTypes = {
+  deleteComment: PropTypes.func.isRequired,
+  comment: PropTypes.object.isRequired,
+  postId: PropTypes.string.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { deleteComment }
+)(CommentItem);
