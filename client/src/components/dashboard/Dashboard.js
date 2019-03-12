e0027bf2edd08dc0a3e3d90af5b4fb7729c15610
unknown <adamalshaick@gmail.com>
2019-03-12 21:03:29 +0100

    refractored and cleaned

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,112 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import {
-  getCurrentProfile,
-  acceptRequest,
-  declineRequest,
-  getProfiles
-} from "../../actions/profileActions";
-
-import { Redirect } from "react-router-dom";
-import styled from "styled-components";
 import ProfileContent from "../profile/ProfileContent";
-import Loading from "../common/Loading";
 import FriendsFeed from "../friends/FriendsFeed";
 import FriendRequests from "../friendRequests/FriendRequests";
+import { Header } from "../common/styles/Header";
+import fetchProfiles from "../common/hoc/fetchProfiles";
 
-import Navbar from "../layout/Navbar";
-
-const Header = styled.header`
-  font-size: 1.5rem;
-  font-weight: 400;
-`;
-
-const Requests = styled.div``;
-
-export class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getCurrentProfile();
-    this.props.getProfiles();
-  }
-
-  onAccept(id) {
-    this.props.onAccept(id);
-  }
-
-  onDecline(id) {
-    this.props.onDecline(id);
-  }
-
-  render() {
-    const { profile, profiles, loading } = this.props.profile;
-    const { user } = this.props.auth;
-
-    let dashboardContent;
-
-    if (profile === null || profiles === null || loading) {
-      dashboardContent = <Loading />;
-    } else {
-      // Check if logged in user has profile data
-      if (Object.keys(profile).length > 0) {
-        dashboardContent = (
-          <>
-            <div className="container entry">
-              {profile.friendRequests && profile.friendRequests.length ? (
-                <Requests>
-                  <FriendRequests profile={profile} profiles={profiles} />
-                </Requests>
-              ) : null}
-
-              <div className="row mt-5">
-                <section
-                  style={{
-                    borderRight: "whitesmoke solid 2px"
-                  }}
-                  className="col-md-6"
-                >
-                  <ProfileContent profile={profile} user={user} />
-                </section>
-                <section className="col-md-6">
-                  <Header className="text-center">Your Friends</Header>
-                  <hr />
-
-                  <FriendsFeed profile={profile} profiles={profiles} />
-                </section>
-              </div>
-            </div>
-          </>
-        );
-      } else {
-        // User is logged in but has no profile
-        this.props.history.push("/create-profile");
-      }
-    }
-    return (
-      <>
-        <Navbar />
-        {dashboardContent}
-      </>
-    );
-  }
-}
+const Dashboard = ({ auth, profile }) => {
+  return (
+    <article className="container entry">
+      {profile.profile.friendRequests &&
+      profile.profile.friendRequests.length ? (
+        <FriendRequests profile={profile.profile} profiles={profile.profiles} />
+      ) : null}
+      <div className="row mt-5">
+        <section
+          style={{
+            borderRight: "whitesmoke solid 2px"
+          }}
+          className="col-md-6"
+        >
+          <ProfileContent profile={profile.profile} user={auth.user} />
+        </section>
+        <section className="col-md-6">
+          <Header className="text-center">Your Friends</Header>
+          <hr />
+          <FriendsFeed profile={profile.profile} profiles={profile.profiles} />
+        </section>
+      </div>
+    </article>
+  );
+};
 
 Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired,
-  getProfiles: PropTypes.func.isRequired,
-  acceptRequest: PropTypes.func.isRequired,
-  declineRequest: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired
 };
 
-const mapStateToProps = state => ({
-  profile: state.profile,
-  profiles: state.profile,
-  auth: state.auth
-});
-
-export default connect(
-  mapStateToProps,
-  { getCurrentProfile, getProfiles, acceptRequest, declineRequest }
-)(Dashboard);
+export default fetchProfiles(Dashboard);
