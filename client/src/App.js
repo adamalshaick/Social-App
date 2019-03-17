fa857f6d4e507114f957f0106d1f8991ba665d84
unknown <adamalshaick@gmail.com>
2019-03-17 04:18:29 +0100

    lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./components/common/PrivateRoute";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile/EditProfile";
-import Profile from "./components/profile/Profile";
-import Feed from "./components/feed/Feed";
-import NotFound from "./components/not-found/NotFound";
 import handleAuth from "./utils/handleAuth";
+import SecondaryLoading from "./components/common/SecondaryLoading";
+import Navbar from "./components/layout/Navbar";
+const Landing = lazy(() => import("./components/layout/Landing"));
+const Register = lazy(() => import("./components/auth/Register"));
+const Dashboard = lazy(() => import("./components/dashboard/Dashboard"));
+const Login = lazy(() => import("./components/auth/Login"));
+const CreateProfile = lazy(() =>
+  import("./components/create-profile/CreateProfile")
+);
+const EditProfile = lazy(() => import("./components/edit-profile/EditProfile"));
+const Profile = lazy(() => import("./components/profile/Profile"));
+const Feed = lazy(() => import("./components/feed/Feed"));
+const NotFound = lazy(() => import("./components/not-found/NotFound"));
 
 const App = () => {
   handleAuth();
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <PrivateRoute exact path="/profile/:handle" component={Profile} />
-        <PrivateRoute exact path="/dashboard" component={Dashboard} />
-        <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-        <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-        <PrivateRoute exact path="/feed" component={Feed} />
-        <Route component={NotFound} />
-      </Switch>
+      <Suspense
+        fallback={
+          <>
+            <Navbar />
+            <SecondaryLoading />
+          </>
+        }
+      >
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/profile/:handle" component={Profile} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute
+            exact
+            path="/create-profile"
+            component={CreateProfile}
+          />
+          <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+          <PrivateRoute exact path="/feed" component={Feed} />
+          <Route component={NotFound} />
+        </Switch>
+      </Suspense>
     </Router>
   );
 };
